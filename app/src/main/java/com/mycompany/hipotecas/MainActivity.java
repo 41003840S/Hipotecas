1cc97978d80b3a1f12a5e75911c7a499565bcf7f
Manuel <41003840s@iespoblenou.org>
2015-10-05 17:15:46 +0200

    Calcula auomaticamente

--- app/src/main/java/com/mycompany/hipotecas/MainActivity.java
+++ app/src/main/java/com/mycompany/hipotecas/MainActivity.java
@@ -2,6 +2,8 @@ package com.mycompany.hipotecas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,15 +12,15 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText preuInmoble, estalvis,plaç, euribor, diferencial;
+    private EditText preuInmoble, estalvis, plaç, euribor, diferencial;
     private TextView mensual, total;
     private double capital, interes, plaç2, cuota;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         preuInmoble = (EditText) findViewById(R.id.ed_preuInmoble);
         estalvis = (EditText) findViewById(R.id.ed_estalvis);
         plaç = (EditText) findViewById(R.id.ed_plaç);
@@ -26,7 +28,14 @@ public class MainActivity extends AppCompatActivity {
         diferencial = (EditText) findViewById(R.id.ed_diferencial);
         mensual = (TextView) findViewById(R.id.tv_mensual);
         total = (TextView) findViewById(R.id.tv_total);
+
+        preuInmoble.addTextChangedListener(watcher);
+        estalvis.addTextChangedListener(watcher);
+        plaç.addTextChangedListener(watcher);
+        euribor.addTextChangedListener(watcher);
+        diferencial.addTextChangedListener(watcher);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -50,19 +59,63 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    TextWatcher watcher = new TextWatcher() {
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            //YOUR CODE
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            //YOUR CODE
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            calcular();
+
+        }
+    };
+
     public void onClick(View view) {
+        calcular();
+    }
 
-        capital = Double.parseDouble(preuInmoble.getText().toString())-Double.parseDouble(estalvis.getText().toString());
-        interes = Double.parseDouble(euribor.getText().toString())+Double.parseDouble(diferencial.getText().toString());
-        plaç2 = Double.parseDouble(plaç.getText().toString())*12;
+    private void calcular() {
+        Calcula calcula = new Calcula().invoke();
+        float mens = calcula.getMens();
+        float tot = calcula.getTot();
+        muestra(mens, tot);
+    }
 
-        cuota = (capital*(interes/12))/(100*(1-(Math.pow((1+(interes/12)/100), -plaç2))));
-
-        float mens = (float) cuota;
-        float tot = (float) (cuota*plaç2);
-
+    private void muestra(float mens, float tot) {
         mensual.setText(String.valueOf(mens));
         total.setText(String.valueOf(tot));
     }
 
+
+    private class Calcula {
+        private float mens;
+        private float tot;
+
+        public float getMens() {
+            return mens;
+        }
+
+        public float getTot() {
+            return tot;
+        }
+
+        public Calcula invoke() {
+            capital = Double.parseDouble(preuInmoble.getText().toString())-Double.parseDouble(estalvis.getText().toString());
+            interes = Double.parseDouble(euribor.getText().toString())+Double.parseDouble(diferencial.getText().toString());
+            plaç2 = Double.parseDouble(plaç.getText().toString())*12;
+
+            cuota = (capital*(interes/12))/(100*(1-(Math.pow((1+(interes/12)/100), -plaç2))));
+
+            mens = (float) cuota;
+            tot = (float) (cuota * plaç2);
+            return this;
+        }
+    }
 }
