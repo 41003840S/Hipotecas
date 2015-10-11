b4af3589e9f9c9290adc77a5f1666bee4181a4b7
Manuel <41003840s@iespoblenou.org>
2015-10-11 04:15:11 +0200

    Spinner, Layouts

--- app/src/main/java/com/mycompany/hipotecas/MainActivity.java
+++ app/src/main/java/com/mycompany/hipotecas/MainActivity.java
@@ -7,10 +7,12 @@ import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView mensual, total, cantidadAnos;
     private double capital, interes, plaç2, cuota;
     private SeekBar plaç;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         total = (TextView) findViewById(R.id.tv_total);
         plaç = (SeekBar) findViewById(R.id.seekBar);
         cantidadAnos = (TextView) findViewById(R.id.cantidadSeek);
+        spinner = (Spinner) findViewById(R.id.spinnerInteres);
 
         euribor.addTextChangedListener(watcher);
         diferencial.addTextChangedListener(watcher);
@@ -40,14 +44,41 @@ public class MainActivity extends AppCompatActivity {
         preuInmoble.addTextChangedListener(watcher);
 
 
-        plaç.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+               switch (position){
+                case 0:
+                    euribor.setEnabled(true);
+                    calcular();
+                    break;
 
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                calcular();
-                cantidadAnos.setText(""+progress);
+                case 1:
+                    euribor.setEnabled(false);
+                    capital = Double.parseDouble(preuInmoble.getText().toString()) - Double.parseDouble(estalvis.getText().toString());
+                    interes = Double.parseDouble(diferencial.getText().toString());
+                    plaç2 = plaç.getProgress() * 12;
+
+                    cuota = (capital * (interes / 12)) / (100 * (1 - (Math.pow((1 + (interes / 12) / 100), -plaç2))));
+
+                   float mens = (float) cuota;
+                   float tot = (float) (cuota * plaç2);
+                    muestra(mens, tot);
+                }
             }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        plaç.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                calcular();
+                cantidadAnos.setText("" + progress);
+            }
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
 
